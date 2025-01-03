7f8479b45438d6d62d2ef0b4bd9a060347847483
Darshan <darshanvaru2003@gmail.com>
2025-01-03 20:09:04 +0530

    Added a global class for showing error or success snack bar

---
+++ lib/services/ProgressNotification.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ProgressNotification {
+  void showErrorSnackBar(String message, BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+        duration: const Duration(seconds: 3),
+      ),
+    );
+  }
+
+  void showSuccessSnackBar(String message, BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.green,
+        duration: const Duration(seconds: 3),
+      ),
+    );
+  }
+}
